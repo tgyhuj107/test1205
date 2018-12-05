fd4dfc535e52771c541982746db0d42c72333079
willy7728 <107021050@gm.asia.edu.tw>
2018-12-05 15:32:09 +0800

    fin

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,61 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+//        int n=scn.nextInt();
+//        float[] data=new float[n];
+//        for (int i=0;i<n;i++){
+//            data[i]=scn.nextFloat();
+//        }
+//        float m=scn.nextFloat(),sum=0;
+//        for (int j=0;j<n;j++){
+//            if (data[j]>=m){
+//                sum+=data[j];
+//            }
+//        }
+//        System.out.printf("%.2f",sum);
+        //01
+        //i am student
+//        String str =scn.nextLine();
+//        String str2="";
+//        char[] t=new char[str.length()];
+//        for (int i=0;i<str.length();i++){
+//            t[i]=str.charAt(i);
+//        }
+//        char a=scn.next().charAt(0);
+//        int b=0;
+//        for (int j=0;j<str.length();j++){
+//            if (t[j]==a){
+//                b++;
+//            }
+//            if (b<=2){
+//                System.out.print(t[j]);
+//            }
+//        }
+
+        //02
         int n=scn.nextInt();
-        float[] data=new float[n];
+        int[] data=new int[n];
         for (int i=0;i<n;i++){
-            data[i]=scn.nextFloat();
+            data[i]=scn.nextInt();
         }
-        float m=scn.nextFloat(),sum=0;
-        for (int j=0;j<n;j++){
-            if (data[j]>=m){
-                sum+=data[j];
+        for (int j =0;j<n;j++){
+            for (int k=j+1;k<n;k++){
+                if (data[j]>data[k]){
+                    int a=data[j];
+                    data[j]=data[k];
+                    data[k]=a;
+                }
             }
         }
-        System.out.printf("%.2f",sum);
-        //01
+        for (int b =0;b<n;b++){
+            System.out.print(data[b]+" ");
+        }
+        System.out.println();
+
+
+
+
+
 
     }
 }
